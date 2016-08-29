49c4e0dcbd8e9ccae0e7a5b75642b5e703f6f8d8
Pinky Sharma <pinky@vidyamantra.com>
2016-08-29 11:40:50 +0530

    Build date update

--- version.php
+++ version.php
@@ -18,7 +18,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_vmchat';
 $plugin->version = 2016082400;
-$plugin->release = '2.0 (Build: 2016082400)';
+$plugin->release = '2.0 (Build: 2016082900)';
 $plugin->requires = 2015111600; // Moodle 3.0 or later.
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = array('local_getkey' => 2014081300);
