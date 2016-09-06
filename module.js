ee274826e71409385ddebb10e269cb3a6db02cd4
Pinky Sharma <pinky@gmail.com>
2016-09-06 17:50:16 +0530

    bug fixing for download

---
+++ module.js
@@ -0,0 +1,12 @@
+function set_vmchat_variable(Y, vmopts) {
+    wwwroot = vmopts.wwwroot;
+    imageurl = vmopts.imageurl;
+    sid = vmopts.sid;
+    auth_user = vmopts.auth_user;
+    auth_pass = vmopts.auth_pass;
+    path = vmopts.path;
+    tk = vmopts.tk;
+    id = vmopts.id;
+    fname = vmopts.fname;
+    lname = vmopts.lname;
+}
